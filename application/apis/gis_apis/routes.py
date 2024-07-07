6b23f35a7082743cad1c8106dbca65fd3e89aa79
del <maulana.fadelm@gmail.com>
2024-07-07 22:24:01 +0800

    feat(area-info): user can get the area information and attributes from provided polygon: - direct polygon input - shapefile input

--- application/apis/gis_apis/routes.py
+++ application/apis/gis_apis/routes.py
@@ -15,8 +15,11 @@ import gc
 import time
 import arrow
 import geopandas
+import pyproj
+import magic
 
-from sqlalchemy.orm import aliased
+from shapely import Polygon, MultiPolygon
+from shapely.ops import transform
 
 from ...utils import AppMessageException, get_date, set_attr, get_default_list_param, exception_handler, success_handler
 from ...utils import eprint, sanitize_psycopg2_query
@@ -83,6 +86,79 @@ def get_district_details(district_name:str):
 
         return make_response(jsonify(success_handler(results)))
     except Exception as e:
+        return make_response(jsonify(exception_handler(e, services='district-details')), 500)
+
+@gis_apis_blueprint.route('/area-polygon', methods=['POST'])
+@cross_origin()
+def post_area_polygon():
+    try:
+        if not current_user.is_authenticated:
+            return make_response(jsonify(exception_handler('not logged in', 401)), 401)
+
+        if not request.is_json:
+            raise AppMessageException('please provide json data')
+        
+        data = request.get_json()
+        if not type(data) == list:
+            raise AppMessageException('json data should be list of list, sample: [[[], [], []]]')
+        
+        polygon = MultiPolygon(data)
+        sgri2013_degree = pyproj.CRS('EPSG:9470')
+        sgri2013_metre = pyproj.CRS('EPSG:9468')
+        project = pyproj.Transformer.from_crs(sgri2013_degree, sgri2013_metre, always_xy=True).transform
+        polygon_metre = transform(project, polygon)
+
+        polygon_gdf = geopandas.GeoDataFrame(geometry=geopandas.GeoSeries(polygon))
+        polygon_gdf.to_postgis('polytemp', con, if_exists='replace')
+
+        gdf = get_intersections()
+
+        results = {
+            # 'area': float(polygon_gdf.area.iloc[0]),
+            # 'length': float(polygon_gdf.length.iloc[0]),
+            'luas': polygon_metre.area,
+            'intersections': gdf.to_dict(orient='records')
+        }
+
+        polygon = None
+        polygon_metre = None
+        polygon_gdf = None
+        gdf = None
+
+        gc.collect()
+
+        return make_response(jsonify(success_handler(results)))
+    except Exception as e:
+        return make_response(jsonify(exception_handler(e, services='city-details')), 500)
+
+@gis_apis_blueprint.route('/area-shapefile', methods=['POST'])
+@cross_origin()
+def post_area_shapefile():
+    try:
+        if not current_user.is_authenticated:
+            return make_response(jsonify(exception_handler('not logged in', 401)), 401)
+
+        shp_file = request.files.get('shp_file')
+        
+        if not shp_file:
+            raise AppMessageException('please provide shp files (shp_file)')
+        
+        polygon_gdf = geopandas.read_file(shp_file)
+        polygon_gdf.crs = 'EPSG:9470'
+        polygon_gdf.to_postgis('polytemp', con, if_exists='replace')
+        polygon_gdf = polygon_gdf.to_crs(epsg=9468)
+
+        gdf = get_intersections(t='shapefile')
+
+        results = {
+            'luas': float(polygon_gdf.area.iloc[0]),
+            'intersections': gdf.to_dict(orient='records')
+        }
+
+        gc.collect()
+
+        return make_response(jsonify(success_handler(results)))
+    except Exception as e:
         return make_response(jsonify(exception_handler(e, services='city-details')), 500)
 
 def getdata(t:str = 'city', args:str = '\0'):
@@ -112,6 +188,36 @@ def getdata(t:str = 'city', args:str = '\0'):
     # set the area units
     gdf['luaswh'] = gdf['geom'].area
 
+    gdf = gdf_sanitize(gdf)
+
+    gdf_meter = None
+    gdf_area_m2 = None
+
+    return True, gdf
+
+def get_intersections(t:str = None):
+    query = '''
+        select
+            b.geom,b.objectid,b.namobj,b.uupp,
+            b.wadmpr,b.wiadkk,b.wadmkk,b.wiadkc,b.wadmkc,
+            b.wiadkd,b.wadmkd
+        from polytemp a
+        left join postgis b on 
+    '''
+
+    if t == 'shapefile':
+        query += ''' ST_Intersects(ST_SetSRID(b.geom, 9470), a.geometry) '''
+    else:
+        query += ''' ST_Intersects(b.geom, a.geometry) '''
+
+    gdf = geopandas.read_postgis(query, con)
+
+    gdf = gdf_sanitize(gdf)
+
+    return gdf
+
+def gdf_sanitize(gdf):
+
     # drop geom data
     gdf = gdf.drop('geom', axis=1)
 
@@ -133,7 +239,4 @@ def getdata(t:str = 'city', args:str = '\0'):
         'luaswh': 'luas',
     }, inplace=True)
 
-    gdf_meter = None
-    gdf_area_m2 = None
-
-    return True, gdf
+    return gdf
